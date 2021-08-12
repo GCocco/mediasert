0599d2e8c9baa13d4a386552ac1137ef96214772
= <=>
2021-08-12 16:14:40 +0200

    added utils file

---
+++ utils.py
@@ -0,0 +1,13 @@
+# stuff
+from enum import IntEnum
+
+class Direction(IntEnum):
+    Forward = 1
+    Back = -1
+    Left = -4
+    Right = 4
+    ForwardRight = Forward + Right
+    ForwardLeft = Forward + Left
+    BackRight = Back + Right
+    BackLeft = Back + Left
+    pass
