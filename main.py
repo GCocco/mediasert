c2ccb83db2838c0b8e7db6e369ef70f42f0b0fe5
= <=>
2021-08-17 16:31:28 +0200

    got rid of debug lines

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 # mediasert game by GCocco
 
 from direct.showbase.ShowBase import ShowBase
-from direct.showbase.DirectObject import DirectObject
-from panda3d.core import NodePath
 
 from config import Loader
 
 from player import FPController
-from maps import EmptyMap, Map_01
+from maps import Map_01
 
 
 class Mediasert(ShowBase):
@@ -16,12 +14,6 @@ class Mediasert(ShowBase):
         super().__init__()
         Loader.init(self.loader)
         self.disableMouse()
-        # debug stuff:
-        smile = self.loader.loadModel("./models/cube.egg")
-        smile.reparentTo(self.render)
-        from models.prefabs import PREFAB_MAP
-        door = PREFAB_MAP["Door_01"]()
-        door.reparentTo(self.render)
         self.accept("escape", exit)
     pass
 
