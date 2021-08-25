9b2f42639035afa64d9de697d5e3a2839eb4fef8
= <=>
2021-08-25 11:47:16 +0200

    material alteration example

--- actors.py
+++ actors.py
@@ -1,6 +1,9 @@
 # actors and actors utilities
 
 from direct.actor.Actor import Actor
+from random import seed, random
+from panda3d.core import LColor
+
 
 class MaleNPC(Actor):
     def __init__(self):
@@ -8,5 +11,7 @@ class MaleNPC(Actor):
                        {"Walk": "./models/actors/male-Walk.egg",
                         "Idle": "./models/actors/male-Idle.egg",
                         "Pain": "./models/actors/male-Pain.egg"})
+        shirtMaterial = self.findMaterial("Shirt")
+        shirtMaterial.setDiffuse(LColor(random(), random(), random(), 1))
         pass
     pass
