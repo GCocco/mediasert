661e29dc0735f9f8728bdf5e74514e7b2a67afc3
= <=>
2021-08-31 18:24:58 +0200

    added example mesh to parse

--- tools/parser.py
+++ tools/parser.py
@@ -2,6 +2,14 @@ from panda3d import egg
 from panda3d.core import Filename
 from math import sqrt
 
+class Vertex:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+        pass
+    pass
+
+
 geom = egg.loadEggFile("./mesh_full.egg").getChild(0).getGeom(0)
 
 # help(geom)
@@ -14,4 +22,5 @@ vdata = geom.getVertexData()
 
 polygons_per_edge = int(sqrt(vdata.getNumRows()) - 1)
 
-print(polygons_per_edge)
+print(polygons_per_edge**2)
+
