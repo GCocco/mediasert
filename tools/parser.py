d69b34c0df23174560fd95b62aef74ea5038b18a
= <=>
2021-08-31 20:14:52 +0200

    vertex data reading

--- tools/parser.py
+++ tools/parser.py
@@ -1,11 +1,12 @@
 from panda3d import egg
-from panda3d.core import Filename
+from panda3d.core import Filename, GeomVertexReader
 from math import sqrt
-
+from pprint import pprint
 class Vertex:
-    def __init__(self, x, y):
-        self._x = x
-        self._y = y
+    def __init__(self, vector):
+        self._x = vector.getX()
+        self._y = vector.getY()
+        self._z = vector.getZ()
         pass
     pass
 
@@ -22,5 +23,24 @@ vdata = geom.getVertexData()
 
 polygons_per_edge = int(sqrt(vdata.getNumRows()) - 1)
 
-print(polygons_per_edge**2)
+# print(polygons_per_edge**2)
 
+print(vdata.getNumRows())
+
+reader = GeomVertexReader(vdata, "vertex")
+
+colonne = set()
+
+while not reader.isAtEnd():
+    colonne.add(reader.getData3().getX())
+    pass
+
+colonne = list(colonne)
+colonne.sort()
+pprint(colonne)
+print("Ignora il primo risultato")
+print(len(colonne))
+p = -100
+for x in colonne:
+    print(p-x)
+    p = x
