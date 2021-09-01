f62bee1363ee80f802af528fd91469d31bb9f31b
= <=>
2021-09-01 15:48:28 +0200

    vertex pool precision error kinda handled

--- tools/parser.py
+++ tools/parser.py
@@ -1,13 +1,92 @@
 from panda3d import egg
-from panda3d.core import Filename, GeomVertexReader
+from panda3d.core import Filename, GeomVertexReader, Vec3
 from math import sqrt
 from pprint import pprint
+
+
 class Vertex:
-    def __init__(self, vector):
-        self._x = vector.getX()
-        self._y = vector.getY()
-        self._z = vector.getZ()
+    TOLLERANCE_X = .00001
+    TOLLERANCE_Y = .00001
+    TOLLERANCE_Z = .001
+    
+    coords_x = set()
+    coords_y = set()
+    coords_z = set()
+    def __init__(self, vector: Vec3):
+
+        closer = self.getCloserX(vector.getX())
+
+        if closer[1] < Vertex.TOLLERANCE_X:
+            self._x = closer[0]
+        else:
+            Vertex.coords_x.add(vector.getX())
+            self._x = vector.getX()
+
+            
+        closer = self.getCloserY(vector.getY())
+        if closer[1] < Vertex.TOLLERANCE_Y:
+            self._y = closer[0]
+        else:
+            Vertex.coords_y.add(vector.getY())
+            self._y = vector.getY()
+
+            
+        closer = self.getCloserZ(vector.getZ())
+        if closer[1] < Vertex.TOLLERANCE_Z:
+            self._z = closer[0]
+        else:
+            Vertex.coords_z.add(vector.getZ())
+            self._z = vector.getZ()
         pass
+    
+
+    def __repr__(self):
+        return f"Vector({self._x},{self._y},{self._z})"
+
+    def __lt__(self, other: "Vertex"):
+        return self._y <= other._y and self._x < other._y
+
+
+    @staticmethod
+    def getCloserX(val):
+        dist = 100
+        closer = 0
+        for coord_x in Vertex.coords_x:
+            distance = abs(val-coord_x)
+            if distance < dist:
+                closer = coord_x
+                dist = distance
+                pass
+            pass
+        return closer, dist
+
+    @staticmethod
+    def getCloserY(val):
+        dist = 100
+        closer = 0
+        for coord_y in Vertex.coords_y:
+            distance = abs(val-coord_y)
+            if distance < dist:
+                closer = coord_y
+                dist = distance
+                pass
+            pass
+        return closer, dist
+
+
+    @staticmethod
+    def getCloserZ(val):
+        dist = 100
+        closer = 0
+        for coord_z in Vertex.coords_z:
+            distance = abs(val-coord_z)
+            if distance < dist:
+                closer = coord_z
+                dist = distance
+                pass
+            pass
+        return closer, dist
+    
     pass
 
 
@@ -29,24 +108,17 @@ print(vdata.getNumRows())
 
 reader = GeomVertexReader(vdata, "vertex")
 
-colonne = set()
+vertexPool = list()
 
 while not reader.isAtEnd():
-    colonne.add(reader.getData3().getX())
+    vertexPool.append(Vertex(reader.getData3()))
     pass
 
-colonne = list(colonne)
-colonne.sort()
-pprint(colonne)
-print("Ignora il primo risultato")
-print(len(colonne))
-p = -100
-count = 0
-for x in colonne:
-    diff = p-x
-    print(diff)
-    if abs(diff) < 0.00000015:  # controllo errore macchina
-        count += 1
-    p = x
+print(len(Vertex.coords_x))
+print(len(Vertex.coords_y))
+print(len(Vertex.coords_z))
 
-print(count)
+
+print("################")
+
+print(Vertex(Vec3(0, 0, 0)) < Vertex(Vec3(1, 1, 1)))
