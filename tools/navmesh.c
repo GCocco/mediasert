3e8033b374b0b89f85cf5a5537269d4980ff5609
= <=>
2021-09-04 12:49:29 +0200

    created navmesh struct and load from file function

--- tools/navmesh.c
+++ tools/navmesh.c
@@ -1,4 +1,5 @@
 #define PY_SSIZE_T_CLEAN
+#include <Python.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -7,46 +8,129 @@
 #include <assert.h>
 #include <stdlib.h>
 
-void main(){
-  FILE * fp = (FILE*) fopen("mynavmesh.csv", "r");
-  int x_num, y_num;
-  int dumb = 0;
-  int dumb2 = 0;
-  
-  fscanf(fp, "%d,%d\n", &x_num, &y_num);
-  printf("x lines: %d\n", x_num);
-  printf("x_lines: %d\n", y_num);
 
-  double x_coords[x_num];
-  double y_coords[y_num];
+typedef struct{
+  int coll_start;
+  int coll_end;
+  struct y_obstacle* next;
+}y_obstacle;   //ok
+
+typedef struct {
+  int start;
+  y_obstacle* obstacles;
+  int end;
+} abc_y;      //
+
+typedef struct {
+  int x_lines;
+  int y_lines;
+  double* x_coords;
+  double* y_coords;
+  abc_y* boundings;
+} nav_mesh;
+
+y_obstacle* append_obstacle(y_obstacle* obs, int start, int end){
+  y_obstacle* new_node = NULL;
+
+  new_node = (y_obstacle*) malloc(sizeof(y_obstacle));
+  if (new_node == NULL){
+    printf("lmao voglio morire");
+  }
   
-  printf("Reading grid coordinates:\n");
+  new_node->next = NULL;
   
-  for (int i=0; i<x_num; i++){
-    fscanf(fp, "%lf,", &x_coords[i]);
-    printf("%lf ", x_coords[i]);
+  new_node->coll_start = start;
+  new_node->coll_end = end;
+  
+
+  if (obs == NULL){
+    return new_node;
   }
 
-  fscanf(fp, "\n");
-  printf("\n");
-  for (int i=0; i<y_num; i++){
-    fscanf(fp, "%lf,", &y_coords[i]);
-    printf("%lf ", y_coords[i]);
+  y_obstacle* head = obs;
+
+  while(head->next != NULL){
+    head = (y_obstacle*) head-> next;
+  }
+
+  head->next = (struct y_obstacle*) new_node;
+  return obs;
+}
+
+nav_mesh load_from_file(char* filename, nav_mesh* nm){
+  FILE* fp = NULL;
+  int dumb1, dumb2, dumb3;
+  fp = (FILE*) fopen(filename, "r");
+  if (fp == NULL){
+    printf("AAAAAAAAAAAAAAAAAAA");
+  }
+
+  fscanf(fp, "%d, %d\n", &nm->x_lines, &nm->y_lines);
+  nm->x_coords = (double*) calloc(nm->x_lines, sizeof(double));
+  for (int i=0; i<nm->x_lines; i++){
+    fscanf(fp, "%lf,", &nm->x_coords[i]);
   }
   
-  printf("\n");
-  printf("loading colliders\n");
+  nm->y_coords = (double*) calloc(nm->y_lines, sizeof(double));
+  for (int i=0; i<nm->x_lines; i++){
+    fscanf(fp, "%lf,", &nm->y_coords[i]);
+  }
 
-  for (int i=0; i<x_num; i++){
-    fscanf(fp, "%d, ", &dumb);
-    for (int j=0; j<(dumb * 2) + 2; j++){
-      fscanf(fp, "%d,", &dumb2);
-      printf("%d ", dumb2);
+  
+  nm->boundings = calloc(nm->x_lines, sizeof(abc_y));
+
+
+  
+  for(int i = 0; i<nm->x_lines; i++){
+    fscanf(fp, "%d,", &dumb1);
+
+    nm->boundings[i].obstacles = NULL;
+    
+    fscanf(fp, "%d,", &nm->boundings[i].start);
+    
+    for(int j = 0; j<dumb1; j++){
+      fscanf(fp, "%d,%d,", &dumb2, &dumb3);
+      nm->boundings[i].obstacles = append_obstacle(nm->boundings[i].obstacles, dumb2, dumb3);
     }
-    printf("\n");
+    fscanf(fp, "%d\n", &nm->boundings[i].end);
   }
-
-  
   fclose(fp);
+}
+
+
+
+
+void main(){
+  nav_mesh culo;
+  load_from_file("mynavmesh.csv", &culo);
+
+
+  if (0){
+    printf("x lines: %d \ny lines: %d\n", culo.x_lines, culo.y_lines);
+    printf("\nx coordinates: \n");
+    for (int i = 0; i < culo.x_lines; i++){
+      printf("%lf\n", culo.x_coords[i]);
+    }
+  }
+  if (0){
+    printf("\ny coordinates: \n");
+    for(int i = 0; i< culo.y_lines; i++){
+      printf("%lf\n", culo.y_coords[i]);
+    }
+  }
+  if (1){
+    printf("\nboundings:\n");
+    for (int i = 0; i< culo.x_lines; i++){
+
+      
+      printf("%d", culo.boundings[i].start);
+      y_obstacle* head = culo.boundings[i].obstacles;
+      while (head != NULL){
+	printf(" {%d, %d}", head->coll_start, head->coll_end);
+	head = (y_obstacle*)head->next;
+      }
+      printf(" %d\n", culo.boundings[i].end);	
+    }
+  }
   
 }
